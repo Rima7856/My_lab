7757882da578ad8e5dd42eda1a55ecb7d9411966
Rima7856 <romalexprokh@gmail.com>
2025-11-05 11:10:59 +0300

    Create new file time.py

---
+++ time.py
@@ -0,0 +1,88 @@
+def what_padej_HS(h):
+    h = h % 12
+    if h == 1:
+        padej = 'час'
+    elif 2 <= h < 5:
+        padej = 'часа'
+    else:
+        padej = 'часов'
+    return padej
+def what_padej_MS(m):
+    rovno = m == 0
+    if 5 <= m <= 20:
+        padej = 'минут'
+    else:
+        last_cou = m % 10
+        if last_cou == 1:
+            padej = 'минута'
+        elif last_cou == 0:
+            padej = 'минут'
+        elif last_cou < 5:
+            padej = 'минуты'
+        else:
+            padej = 'минут'
+    return rovno, padej
+def what_vrem_sut(h):
+    if h == 0:
+        return 12, 'ночи'
+    elif 1 <= h <= 5:
+        time, v_s = str(h), 'ночи'
+    elif 6 <= h <= 11:
+        time, v_s = str(h), 'утра'
+    elif 12 == h:
+        time, v_s = str(h), 'дня'
+    elif 13 <= h <= 17:
+        time, v_s = str(h % 12) , 'дня'
+    else:
+        time, v_s = str(h % 12) , 'вечера'
+    return time, v_s
+def err(x):
+    A = x.split()
+    if len(A) != 2:
+        return True, 'Введите ровно два числа (часы и минуты) через пробел'
+    err_h = not (A[0].isdigit())
+    err_m = not (A[1].isdigit())
+    if not err_m:
+        minutes = int(A[1])
+        err_m = not (0 <= minutes <= 59)
+    if not err_h:
+        hours = int(A[0])
+        err_h = not (0 <= hours <= 23)
+    is_err = err_m or err_h
+    text = ''
+
+    if is_err:
+        input_h_err = 'часы должны быть от 0 до 23'
+        input_m_err = 'минуты должны быть от 0 до 59'
+        text = 'Введены недопустимые данные: '
+        if err_h and err_m:
+            text += f'{input_h_err}, {input_m_err}'
+        elif err_m:
+            text += input_m_err
+        else:
+            text += input_h_err
+
+    return is_err, text
+def main_fk(x):
+    err_T, text = err(x)
+    if err_T:
+        return text
+    hours, minutes = map(int, x.split())
+
+
+
+
+    padej_h = what_padej_HS(hours)
+    rovno, padej_m = what_padej_MS(minutes)
+    new_hours, vrem_sut = what_vrem_sut(hours)
+
+    if hours == 0 and minutes == 0:
+        return 'полночь'
+    elif hours == 12 and minutes ==0:
+        return 'полдень'
+    if rovno:
+        return f'{new_hours} {padej_h} {vrem_sut} ровно'
+    else:
+        return f'{new_hours} {padej_h} {minutes} {padej_m} {vrem_sut}'
+
+print(main_fk(input()))
